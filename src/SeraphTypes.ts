7197088bcdb67acef79603fb0395e686c8b5794d
kk <kk>
2022-12-12 04:05:32 +0000

    refactor: added proper types
    Added a union type for isKeyValid and getPlayerBlacklist

---
+++ src/SeraphTypes.ts
@@ -0,0 +1,91 @@
+export type Blacklist = {
+	data: {
+		uuid: string;
+		username: string;
+		blacklist: {
+			tagged: boolean;
+			timestamp: number;
+			reason: string;
+			report_type: string;
+		};
+		safelist: {
+			tagged: boolean;
+			timesKilled: number;
+			personal: boolean;
+			security_level: number;
+		};
+		customTag: string | null;
+		bot: { tagged: boolean; unidentified: boolean; kay: boolean };
+		statistics: { encounters: number; threat_level: number };
+		migrated: { tagged: boolean };
+		annoylist: { tagged: boolean };
+	};
+} & SeraphDefaultStructure;
+
+export type ApiKey = {
+	key: {
+		key: string;
+		valid: boolean;
+		code: number;
+		isDev: boolean;
+	};
+} & SeraphDefaultStructure;
+
+export type LunarCosmetic = {
+	id: number;
+	name: string;
+	url: string;
+};
+
+export type LunarAPIResponse = {
+	success: boolean;
+	code: number;
+	player: {
+		uuid: string;
+		online: boolean;
+		status: string;
+		cosmetics: { count: number; activeCosmetics: LunarCosmetic[]; cachedCosmetics: LunarCosmetic[] };
+		lunarPlus: {
+			premium: boolean;
+			clothCloak: boolean;
+			plusColour: number;
+		};
+		rank: {
+			unknownBooleanB: boolean;
+			unknownBooleanC: boolean;
+		};
+		unknown: {
+			unknownBooleanA: boolean;
+			unknownBooleanB: boolean;
+			unknownBooleanC: boolean;
+		};
+	};
+	msTime: number;
+};
+
+export type SeraphResponse<T> = Promise<T | ErrorResponse>;
+
+export type ErrorResponse = {
+	success: false;
+	code: number;
+	msTime: number;
+	cause: string;
+	extra: Array<ErrorResponseExtra>;
+	documentation: string;
+};
+
+export type ErrorResponseExtra = {
+	name: string;
+	reason: string;
+	developer_reason?: string;
+	code?: number;
+};
+
+export type SeraphDefaultStructure =
+	| ({
+			code: number;
+			msTime: number;
+	  } & {
+			success: true;
+	  })
+	| ErrorResponse;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,88 @@
+import axios from "axios";
+import { ApiKey, Blacklist, ErrorResponse, ErrorResponseExtra, SeraphResponse } from "./SeraphTypes";
+
 export type ClientOptions = {
-  /**
-   *  Seraph API Key
-   */
-  apiKey: string
-  /**
-   *  Set a custom user agent when making calls to the Seraph API
-   */
-  userAgent?: string
-}
+	/**
+	 *  Seraph API Key
+	 */
+	apiKey: string;
+	/**
+	 *  Set a custom user agent when making calls to the Seraph API
+	 */
+	userAgent?: string;
+	/**
+	 * Set a timeout. Default is 5 seconds ( 5000 ms )
+	 */
+	timeout?: number;
+};
 
 export class SeraphApi {
+	private apiKey;
+	private readonly axiosInstance;
 
-  private apiKey;
-  private isKeyLocked = false;
+	constructor(options: ClientOptions) {
+		this.apiKey = options.apiKey;
+		this.axiosInstance = axios.create({
+			headers: {
+				"run-api-key": options.apiKey,
+				"User-Agent": options?.userAgent ?? "seraph-library",
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+			baseURL: "https://api.seraph.si/",
+			timeout: options?.timeout ?? 5000,
+			timeoutErrorMessage: JSON.stringify({
+				success: false,
+				cause: `I have timed out. Response took longer than ${options?.timeout ?? 5000} milliseconds.`,
+				code: 500,
+				extra: [
+					{
+						name: "Timeout",
+						code: 500,
+						reason: "The library has timed out.",
+						developer_reason: `Please check you're connected to the internet and the Seraph API is up. If the API is down, Contact us via discord immedietly.`,
+					},
+				],
+				documentation: "https://api.seraph.si",
+				msTime: Date.now(),
+			}),
+			validateStatus: () => true,
+		});
+	}
 
-  constructor(options: ClientOptions) {
-    this.apiKey = options.apiKey;
-  }
+	public async isKeyValid(): SeraphResponse<ApiKey> {
+		const { data } = await this.axiosInstance.get<ApiKey>("/key");
+		return data;
+	}
 
+	public async getPlayerBlacklist(uuid: string): SeraphResponse<Blacklist> {
+		if (this.validateUuid(uuid)) {
+			const { data } = await this.axiosInstance.get<Blacklist>(`/blacklist?uuid=${uuid}`);
+			return data;
+		} else {
+			return this.validateErrorFromMethod(`The UUID provided didn't match`);
+		}
+	}
+
+	private validateUuid(uuid: string) {
+		return uuid.match(/^[0-9a-f]{32}$/);
+	}
+
+	private validateErrorFromMethod(cause: string): ErrorResponse {
+		const extra: Array<ErrorResponseExtra> = [
+			{
+				name: "Invalid Request",
+				code: 400,
+				reason: cause,
+			},
+		];
+		return {
+			success: false,
+			code: 400,
+			cause,
+			extra,
+			documentation: "https://api.seraph.si",
+			msTime: Date.now(),
+		};
+	}
 }

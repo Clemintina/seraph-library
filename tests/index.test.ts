60de4ee18fbeddda8600858a966e7ff697d324d5
kk <kk>
2022-12-12 04:04:27 +0000

    feat: added tests
    Added tests for valid key and blacklist requests
    Added tests for failing key and blacklist requests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,26 @@
 import { SeraphApi } from "../src";
 
-const apiKey = "public";
+const PLAYER_UUID  = 'b9aa26ec20b447a59a4ad10cc08a46fe' // Arman
+const API_KEY = "public";
 
-test('validate key', async ()=>{
-	const testApi = new SeraphApi({
-		apiKey
+test('validate key & blacklist', async ()=>{
+	const publicSeraphApi = new SeraphApi({
+		apiKey:API_KEY
 	})
 
-	const keyResponse = await testApi.isKeyValid()
-	if ( keyResponse.success ) {
-		console.log(keyResponse);
-	}
+	const keyResponse = await publicSeraphApi.isKeyValid()
+	expect(keyResponse.success).toBe(true)
 
+	const blacklistResponse = await publicSeraphApi.getPlayerBlacklist(PLAYER_UUID)
+	expect(blacklistResponse.success).toBe(true)
+})
+
+test('Fail key & blacklist', async ()=>{
+	const failingSeraphApi = new SeraphApi({ apiKey : ""})
+
+	const failedKeyResponse = await failingSeraphApi.isKeyValid()
+	expect(failedKeyResponse.success).toBe(false)
+
+	const failedBlacklistResponse = await failingSeraphApi.getPlayerBlacklist(PLAYER_UUID)
+	expect(failedBlacklistResponse.success).toBe(false)
 })

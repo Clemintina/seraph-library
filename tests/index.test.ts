ecbd12ef7168d408d91dcc4941326103c6d3dac6
kk <git@kayplugin.si>
2022-12-13 10:57:39 +0000

    style: ran prettier

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,51 +1,51 @@
 import { SeraphApi } from "../src";
 
-const PLAYER_UUID  = 'b9aa26ec20b447a59a4ad10cc08a46fe' // Arman
+const PLAYER_UUID = "b9aa26ec20b447a59a4ad10cc08a46fe"; // Arman
 const API_KEY = "public"; // Seraph key
 
-test('validate key & blacklist', async ()=>{
+test("validate key & blacklist", async () => {
 	const publicSeraphApi = new SeraphApi({
-		apiKey:API_KEY
-	})
+		apiKey: API_KEY
+	});
 
-	const keyResponse = await publicSeraphApi.isKeyValid()
-	expect(keyResponse.success).toBe(true)
+	const keyResponse = await publicSeraphApi.isKeyValid();
+	expect(keyResponse.success).toBe(true);
 
-	const blacklistResponse = await publicSeraphApi.getPlayerBlacklist(PLAYER_UUID)
-	expect(blacklistResponse.success).toBe(true)
-})
+	const blacklistResponse = await publicSeraphApi.getPlayerBlacklist(PLAYER_UUID);
+	expect(blacklistResponse.success).toBe(true);
+});
 
-test('Fail key & blacklist', async ()=>{
-	const failingSeraphApi = new SeraphApi({ apiKey : ""})
+test("Fail key & blacklist", async () => {
+	const failingSeraphApi = new SeraphApi({ apiKey: "" });
 
-	const failedKeyResponse = await failingSeraphApi.isKeyValid()
-	expect(failedKeyResponse.success).toBe(false)
+	const failedKeyResponse = await failingSeraphApi.isKeyValid();
+	expect(failedKeyResponse.success).toBe(false);
 
-	const failedBlacklistResponse = await failingSeraphApi.getPlayerBlacklist(PLAYER_UUID)
-	expect(failedBlacklistResponse.success).toBe(false)
-})
+	const failedBlacklistResponse = await failingSeraphApi.getPlayerBlacklist(PLAYER_UUID);
+	expect(failedBlacklistResponse.success).toBe(false);
+});
 
-test('Testing the lunar api', async ()=>{
+test("Testing the lunar api", async () => {
 	// No API key is needed as the API is available to everyone
-	const seraphApi = new SeraphApi({ apiKey : ""})
-	const lunarResponse = await seraphApi.getPlayerLunar( PLAYER_UUID );
-	expect(lunarResponse.success).toBe(true)
-})
+	const seraphApi = new SeraphApi({ apiKey: "" });
+	const lunarResponse = await seraphApi.getPlayerLunar(PLAYER_UUID);
+	expect(lunarResponse.success).toBe(true);
+});
 
-test('Testing player finder api', async ()=>{
-	const seraphApi = new SeraphApi({ apiKey : API_KEY})
-	const response = await seraphApi.getPlayerFinder()
-	expect(response.success).toBe(true)
-})
+test("Testing player finder api", async () => {
+	const seraphApi = new SeraphApi({ apiKey: API_KEY });
+	const response = await seraphApi.getPlayerFinder();
+	expect(response.success).toBe(true);
+});
 
-test('Handle security alert', async ()=>{
-	const seraphApi = new SeraphApi({apiKey: ''})
+test("Handle security alert", async () => {
+	const seraphApi = new SeraphApi({ apiKey: "" });
 	const response = await seraphApi.isKeyValid();
-	expect(response).toBe(false)
-	if ( response.code == 200){
-		expect(response.success).toBeDefined()
-		expect(response.key.valid).toBeDefined()
-	}else {
-		expect(response.cause).toBeDefined()
+	expect(response).toBe(false);
+	if (response.code == 200) {
+		expect(response.success).toBeDefined();
+		expect(response.key.valid).toBeDefined();
+	} else {
+		expect(response.cause).toBeDefined();
 	}
-})
+});
